2a49707e48cc07953104dc82444fe49ba5cff711
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-08-06 00:39:20 +0100

    test: add unit tests for success and failure cases

--- __tests__/batch-request.test.js
+++ __tests__/batch-request.test.js
@@ -1,9 +1,46 @@
+const { batchRequest } = require('../batch-request')
+
 describe('batch-request', () => {
   describe('successful requests', () => {
-    it('should return successful responses', () => {})
+    it('should return successful responses', async () => {
+      // assign
+      const data = ["customer_id_100", "customer_id_101"]
+      const request = customerId => Promise.resolve({ customerId })
+      // act
+      const result = await batchRequest(data, request)
+      // assert
+      expect(result).toMatchObject({
+        batchFailed: [],
+        batchNumberFailed: [],
+        batchSucceeded: [
+          "customer_id_100",
+          "customer_id_101"
+        ],
+        response: [
+          { customerId: "customer_id_100" },
+          { customerId: "customer_id_101" }
+        ]
+      })
+    })
   })
 
   describe('failing requests', () => {
-    it('should return failed requests', () => {})
+    it('should return failed requests', async () => {
+      // assign
+      const data = ["customer_id_100", "customer_id_101"]
+      const request = customerId => Promise.reject({ customerId })
+      // act
+      const result = await batchRequest(data, request)
+      // assert
+      expect(result).toMatchObject({
+        batchFailed: [
+          "customer_id_100",
+          "customer_id_101"
+        ],
+        batchNumberFailed: [0],
+        batchSucceeded: [],
+        response: []
+      })
+    })
   })
 })
