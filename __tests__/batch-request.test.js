9a4c4c79d3a0ff4b29492d7531a770f109915028
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-08-05 22:52:15 +0100

    test: detail test cases

--- __tests__/batch-request.test.js
+++ __tests__/batch-request.test.js
@@ -1,9 +1,9 @@
 describe('batch-request', () => {
   describe('successful requests', () => {
-
+    it('should return successful responses', () => {})
   })
 
   describe('failing requests', () => {
-    
+    it('should return failed requests', () => {})
   })
 })
