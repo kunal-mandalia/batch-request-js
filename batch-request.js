2a2fa20fc45939f30cd140465dfda290a3d2c961
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-08-16 01:32:24 +0100

    chore: document function signature

--- batch-request.js
+++ batch-request.js
@@ -3,10 +3,10 @@
  * Batch async requests, throttled with a delay
  * to avoid hammering the network
  *  
- * @param {Array} data 
- * @param {Function} request 
+ * @param {Array} records 
+ * @param {Function} request (returns Promise)
  * @param {Object} options 
- * @returns {Object} result
+ * @returns {Object} result { error, data }
  */
 const batchRequest = (records, request = () => {}, options = { batchSize: 100, delay: 100 }) => {
   return new Promise(async resolve => {
@@ -16,7 +16,7 @@ const batchRequest = (records, request = () => {}, options = { batchSize: 100, d
 
     for (let i = 0; i < records.length; i += options.batchSize) {
       const batch = records.slice(i, i + options.batchSize)
-      // capture individual errors 
+      // capture individual errors
       // as per https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all#Promise.all_fail-fast_behaviour
       const result = await Promise.all(
         batch.map(record => request(record).catch(e => ({ record, error: new Error(e) })))
@@ -24,6 +24,7 @@ const batchRequest = (records, request = () => {}, options = { batchSize: 100, d
       response = response.concat(result)
       await delay(options.delay)
     }
+    // separate successful requests from errors
     response.forEach(res => {
       res && (res.error instanceof Error) ? error.push(res) : data.push(res)
     })
@@ -35,7 +36,6 @@ const batchRequest = (records, request = () => {}, options = { batchSize: 100, d
 }
 
 const delay = (ms = 150) => new Promise(resolve => setTimeout(resolve, ms))
-
 
 module.exports = {
   batchRequest,
